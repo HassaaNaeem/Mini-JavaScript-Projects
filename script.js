b258cdae13247a62aff8ba9a661eebe8efc6057c
user.name like: "HassaaNaeem <user.email like: "hassaan282006@gmail.com>
2025-08-08 20:39:02 +0500

    Updated

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ panels.forEach(function (panel) {
     panel.classList.add("active");
   });
 });
+
 function removeActiveClasses() {
   panels.forEach((panel) => {
     panel.classList.remove("active");
